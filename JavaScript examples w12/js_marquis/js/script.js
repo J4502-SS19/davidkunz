abc518c3c9e03dba82655352dc54f739a731b35c
David Kunz <dakz64@mail.missouri.edu>
2019-04-11 10:51:58 -0500

    Java
    Updating java script in calss

---
+++ JavaScript examples w12/js_marquis/js/script.js
@@ -0,0 +1,24 @@
+var newLink = document.createElement('a');
+
+var allParagraphs = document.getElementsByTagName('p');
+
+var firstParagraph = allParagraphs[0];
+
+newLink.setAttribute('href','#');
+
+newLink.innerHTML="Read More";
+
+firstParagraph.appendChild ( newLink );
+
+for ( var i=0; i < allParagraphs.length; i++){
+  if(i==0) {
+    continue;
+  }
+  allParagraphs[i].style.display='none';
+  }
+
+newLink.setAttribute('class','more-link');
+
+//newLink.style.display ='inline-block';
+
+//newLink.style.marginLeft='0.5em';
---
+++ JavaScript examples w12/lecture_examples/script.js
@@ -0,0 +1,7 @@
+var foo=10;
+var bar=20;
+if (foo < bar) {
+  console.log("foo is less than bar");
+  console.log(foo);
+  console.log(bar);
+}
